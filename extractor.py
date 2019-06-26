b6e40d7dfeb2bba6a18ffa7b7478b0b8413b0f4d
Chris Gora <kg17815@my.bristol.ac.uk>
2019-06-26 17:07:37 +0100

    Added csv file output

--- extractor.py
+++ extractor.py
@@ -1,15 +1,21 @@
+import csv
 import re
+from pathlib import Path
 
 import docx2txt
-from pathlib import Path
 
 if __name__ == '__main__':
     excluded = []
-    emails = []
+    emails = set()
     for filename in Path('docs').glob('**/*.docx'):
         text = docx2txt.process(filename)
         for email in re.findall(r'\w+@\w+\.\w+\.*\w*', text):
             if email not in excluded:
-                emails.append(email)
+                emails.add(email)
 
     print(emails)
+
+    with open('emails.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        for email in emails:
+            writer.writerow([email])
